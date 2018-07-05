aae40602c14523d1fe07a7da90dc0cdf615c2be5
Tan Shane Lee <tanshanelee@gmail.com>
2018-07-05 15:36:13 +0800

    Fixed bugs. Refer to readme for list of changes.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -19,6 +19,7 @@ TodoList.propTypes = {
   todos: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.number.isRequired,
     completed: PropTypes.bool.isRequired,
+    text: PropTypes.string.isRequired
   }).isRequired).isRequired,
   toggleTodo: PropTypes.func
 }
